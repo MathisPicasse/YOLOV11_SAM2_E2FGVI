d7751097f2d6190a2f96431e5e393d96a29c5d28
Mathis Picasse <mathispicasse@gmail.com>
2025-07-08 11:45:54 +0200

    modified the configuration of the dataset

--- tests/test_convert_to_yolo.py
+++ tests/test_convert_to_yolo.py
@@ -11,16 +11,16 @@ from typing import Dict, Tuple
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # --- Configuration ---
-JSON_CONFIG_FILE = "/home/mapicasse/Documents/02_Academic/Internship/YOLO11_SAM_E2FGVI/configs/MOT_TO_YOLO.json"
+JSON_CONFIG_FILE = "/home/mapicasse/Documents/02_Academic/Internship/YOLO11_SAM_E2FGVI/configs/MOT_TO_YOLO_test.json"
 
 # Define output paths for this test script (can be different from main conversion)
 # These are example paths; adjust as needed.
 # It's often good to use a dedicated 'test_outputs' directory.
-TEST_IMG_DIR = "/home/mapicasse/Documents/02_Academic/Internship/YOLO11_SAM_E2FGVI/training/val/MOT17-09_yolo/images"
-TEST_ANNOTATIONS_DIR = "/home/mapicasse/Documents/02_Academic/Internship/YOLO11_SAM_E2FGVI/training/val/MOT17-09_yolo/labels"
-TEST_OUTPUT_VIDEO = "/home/mapicasse/Documents/02_Academic/Internship/YOLO11_SAM_E2FGVI/outputs/test_MOT17-09_yolo_annotated.mp4"
-VIDEO_PREFIX = "MOT17-09" # This should match the prefix of your images in TEST_IMG_DIR
-VIDEO_FPS = 30
+TEST_IMG_DIR = "/home/mapicasse/Documents/02_Academic/Internship/YOLO11_SAM_E2FGVI/training/test/MOT20-03_yolo/images"
+TEST_ANNOTATIONS_DIR = "/home/mapicasse/Documents/02_Academic/Internship/YOLO11_SAM_E2FGVI/training/test/MOT20-03_yolo/labels"
+TEST_OUTPUT_VIDEO = "/home/mapicasse/Documents/02_Academic/Internship/YOLO11_SAM_E2FGVI/outputs/test_MOT20-03_yolo_annotated.mp4"
+VIDEO_PREFIX = "MOT20-03" # This should match the prefix of your images in TEST_IMG_DIR
+VIDEO_FPS = 15
 VIDEO_CODEC = 'mp4v' # Common codec for .mp4 files
 
 def generate_class_colors(class_names: Dict[int, str]) -> Dict[int, Tuple[int, int, int]]:
